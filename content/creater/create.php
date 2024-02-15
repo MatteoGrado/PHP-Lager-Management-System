14e65a038df85f055230d872ae42e2379c9d1e96
Matteo Grado <111707247+MatteoGrado@users.noreply.github.com>
2024-02-15 15:34:53 +0100

    redesign and changes in the src directory
    I deleted the Barcode Controller for remake. I hope tomorrow I can finish the create feture!

--- content/creater/create.php
+++ content/creater/create.php
@@ -1,5 +1,13 @@
 <?php
 
+    use App\PHPLMS\src\Controller\BarcodeGeneratorController;
+    include '../../src/Controller/BarcodeGeneratorController.php';
+
+    if (!file_exists('C:\Users\matteo.grado\Ampps\www\Workspace\PHP-Lager-Management-System\src\Controller\BarcodeGeneratorController.php')) {
+        echo "File not found!";
+    }
+
+    $generator = new BarcodeGeneratorController();
 ?>
 <!doctype html>
 <html lang="en">
@@ -21,22 +29,28 @@
     <form class="input-form" method="POST">
         <label class="lab0">Produkt Nummer</label>
             <input class="inp0" type="number" name="product_number">
-        <label class="lab1">Prüfziffer</label>
-            <input class="inp1" type="number" name="check_num">
-        <label class="lab2">Erstellt von</label>
-            <input class="inp2" type="text" name="creator" required>
-        <label class="lab02">Zähler</label>
-            <input class="inp02" type="number" name="counter" autocomplete="true">
-        <label class="lab3">Erstellt am</label>
-            <input class="inp3" type="datetime-local" name="time" autocomplete="true">
-        <img src="../../public/assets/img/PNG/sub-barcodeImage.png" class="img1" alt="Image-Barcode">
+        <label class="lab1">Beschriftung</label>
+            <input class="inp1" type="text" name="description">
+        <label class="lab2">Barcode-Typ</label>
+            <select class="barcode-select-type">
+                <option value="C128">Code 128</option>
+                <option value="C128A">Code 128 A</option>
+                <option value="C128B">Code 128 B</option>
+                <option value="C39">Code 39</option>
+                <option value="C39E">Code 39 E</option>
+                <option value="C93">Code 93</option>
+                <option value="EAN8">EAN 8</option>
+                <option value="EAN13">EAN 13</option>
+            </select>
+        <div class="img-holder">
+            <img
+                src="data:image/jpg;base64,<?php base64_decode($generator->getBarcode());?>"
+                alt="Barcode-Image"class="barcode-image">
+            <button type="submit" class="btn1" name="print">Drucken</button>
+            <button type="submit" class="btn2" name="download">Download</button>
+        </div>
         <button type="submit" class="btn0" name="create">Erstellen</button>
     </form>
-        <div class="border-h1"></div>
-    <div class="functions">
-        <button type="submit" class="btn1">PDF-Erstellen</button>
-        <button type="submit" class="btn2">Alle Barcodes Drucken</button>
-    </div>
         <div class="border-v1"></div>
     <table class="table">
         <tr>
@@ -46,10 +60,10 @@
             <th>Bestand</th>
         </tr>
         <tr>
-            <td>001</td>
-            <td>8627194</td>
-            <td>Drucker</td>
-            <td>0001</td>
+            <td><?php //vars ?></td>
+            <td><?php //vars ?></td>
+            <td><?php //vars ?></td>
+            <td><?php //vars ?></td>
         </tr>
     </table>
 </body>
--- src/Controller/BarcodeGeneratorController.php
+++ src/Controller/BarcodeGeneratorController.php
@@ -6,19 +6,7 @@
     use Picqer\Barcode\BarcodeGeneratorJPG;
 
     class BarcodeGeneratorController {
-        public function createBarcode(): mixed {
-            $db = new DB("localhost", "Admin", "Sumafelo03!", "php_lms");
-            if (!empty('product_number')) {
-                $product_num = $_POST['product_number'];
+        public function getBarcode() {
 
-                $sql = "SELECT 'product_number, 'product_name', 'storage' FROM 'products'";
-                foreach ($db->con->query($sql) as $data) {
-                    return $data;
-                }
-
-                $generator = new BarcodeGeneratorJPG();
-            } else {
-                echo "Bitte geben Sie eine Produktnummer ein!";
-            }
         }
     }
